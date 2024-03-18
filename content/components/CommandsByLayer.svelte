d64bab20babe718d4024f6944a173254e77e3ce7
Andrew Wong <andrew.j.wong@outlook.com>
2024-03-19 02:55:13 +1100

    Command category filter

--- content/components/CommandsByLayer.svelte
+++ content/components/CommandsByLayer.svelte
@@ -7,20 +7,39 @@
     type CommandEntry,
   } from "./tsk_commands";
 
-  let commandsbyLayer: { [layer in LayerTypes]: CommandEntry[] } = tsk_commands
+
+
+  export let interactCategory: boolean = true
+
+  let commandsByLayer: { [layer in LayerTypes]: CommandEntry[] } = tsk_commands
     .filter(({ layer }) => !!layer)
     .reduce(
       (obj, cur) => ({ ...obj, [cur.layer]: [...(obj[cur.layer] ?? []), cur] }),
       <any>{}
     );
 
-  const visibleLayers: LayerTypes[] = ["disk", "blk"];
+
+  const visibleLayers: LayerTypes[] = [
+    "automated",
+    "fs",
+    "fn",
+    "meta",
+    "blk",
+    "fsj",
+    "vol",
+    "img",
+    "disk",
+    "misc",
+  ];
+
+  visibleLayers.forEach((layer) => {
+    if (!commandsByLayer[layer]) {
+      throw new Error(`No commands for layer [${layer}]`);
+    }
+  });
 </script>
 
 {#each visibleLayers.filter((v) => !!v) as layer (layer)}
-  <h4>{Layer[layer]}</h4>
-  <CommandGroup data={commandsbyLayer[layer]} showLayer={false} />
+  <h3>{Layer[layer]}</h3>
+  <CommandGroup {interactCategory} on:category data={commandsByLayer[layer]} showLayer={false} />
 {/each}
-
-<!-- <CommandGroup data={commandsbyLayer["automated"]} /> -->
-<!-- <CommandGroup data={commandsbyLayer["blk"]} /> -->
